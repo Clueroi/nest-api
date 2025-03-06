189d165d228debe1b8b4e146ea9ecbefb6ba98a2
clueroi <eriicsouzaromeroo@gmail.com>
2025-03-06 10:15:15 -0300

    Feat: created create account controller

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,37 @@
+import { Body, ConflictException, Controller, Post } from "@nestjs/common";
+import { hash } from "bcryptjs";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) { }
+
+  @Post()
+  async handle(
+    @Body() body: any
+  ) {
+
+    const {name, email, password} = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where:{
+        email
+      }
+    })
+    
+    if(userWithSameEmail){
+      throw new ConflictException('User with same email address')
+    }
+
+    const hashedPassword = await hash(password, 8)
+    
+    await this.prisma.user.create({
+      data:{
+        name,
+        email,
+        password:hashedPassword
+      }
+    })
+  }
+}
+
