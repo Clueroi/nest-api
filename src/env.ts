d6ca779a5a8178d61ba78343f01106c33f101239
clueroi <eriicsouzaromeroo@gmail.com>
2025-03-14 19:20:34 -0300

    Feat: Auth Module configured to receive Environment Variables and authenticate user, using passport nestJs and jwt nestjs

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,8 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   DATABASE_URL: z.string(),
-  PORT: z.coerce.number().optional().default(3000)
+  PORT: z.coerce.number().optional().default(3000),
+  JWT_SECRET:z.string()
 })
 
 export type Env = z.infer<typeof envSchema>
