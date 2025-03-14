d32f9915b669d009c949b230848859d412311346
clueroi <eriicsouzaromeroo@gmail.com>
2025-03-14 20:08:03 -0300

    Feat: Auth module using jwt gets configured with private and public keys, app module received the auth module and were creted a authentic controller to tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { envSchema } from './env';
 import { AuthModule } from './auth/auth.module';
+import { AuthenticateController } from './controllers/authenticate.controller';
 
 @Module({
   imports: [
@@ -12,9 +13,8 @@ import { AuthModule } from './auth/auth.module';
     isGlobal: true
   }),
   AuthModule,
-  
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule { }
